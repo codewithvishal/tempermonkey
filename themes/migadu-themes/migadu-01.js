6d37a189348e91c3e5bc887e8153bf98c18241b8
Vishal Chopra <chopra.vishal525@gmail.com>
2020-08-20 12:23:13 +0530

    Update migadu-01.js

--- themes/migadu-themes/migadu-01.js
+++ themes/migadu-themes/migadu-01.js
@@ -24,3 +24,7 @@ addGlobalStyle('.thm-body { background: #ffffff !important; }');
 addGlobalStyle('.thm-folders .e-link.selectable.selected { color: #d93025 !important; background-color: #fce8e6 !important; border-radius: 0 20px 20px 0;}');
 addGlobalStyle('html.ssm-state-desktop #rl-right {left: 225px;}')
 addGlobalStyle('.thm-folders .e-link.selectable:hover {color: #444 !important;background-color: #dedede !important;border-radius: 0 20px 20px 0;}')
+addGlobalStyle('.messageList .b-message-list-wrapper{box-shadow: 0 0px 0px rgba(0,0,0,0.2);-webkit-box-shadow: 0 0px 0px rgba(0,0,0,.2);}')
+addGlobalStyle('.messageView .b-content, .messageView.message-focused .b-content {box-shadow: 0 0px 0px rgba(0,0,0,0.2) !important;}')
+
+
