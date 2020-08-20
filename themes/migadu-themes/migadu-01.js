911874806c65f8b640875914ec84cf6c8e50bc34
Vishal Chopra <chopra.vishal525@gmail.com>
2020-08-20 11:50:07 +0530

    Update migadu-01.js

--- themes/migadu-themes/migadu-01.js
+++ themes/migadu-themes/migadu-01.js
@@ -1,1 +1,21 @@
 // Migadu theme 01
+// ==UserScript==
+// @name         Migadu Theme 01
+// @namespace    
+// @version      Version Death
+// @description  A theme for Migadu
+// @match        https://webmail.migadu.com/*
+// @author       CodeWithVishal
+// @icon         
+// @grant        none
+// ==/UserScript==
+
+function addGlobalStyle(css) {
+    var head, style;
+    head = document.getElementsByTagName('head')[0];
+    if (!head) { return; }
+    style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = css;
+    head.appendChild(style);
+}
